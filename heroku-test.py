0d0f656f1877a8f24d53ad347c1d22ce74018c60
unknown <f158140@nu.edu.pk>
2020-08-11 13:39:48 +0500

    make it better

--- heroku-test.py
+++ heroku-test.py
@@ -16,7 +16,7 @@ driver.get("https://jumpstory.com/remove-background/")
 sleep(2)
 # temp = driver.find_elements_by_xpath('//*[@id="page-content"]/div[1]/div/div/div[1]/div/div[1]')
 
-file=driver.find_element_by_xpath('//*[@id="content"]/div/div/div/section[3]/div/div/div/div/div/div/div/div/div/div/div/p[1]/label/input').send_keys(r'C:\Users\TanzeelKhan\PycharmProjects\dynamicCrawl\beautiful-1274361_1920.jpg')
+file=driver.find_element_by_xpath('//*[@id="content"]/div/div/div/section[3]/div/div/div/div/div/div/div/div/div/div/div/p[1]/label/input').send_keys(r'https://cocainemodels.com/storage/2018/12/model-agentur-anschreiben-auftreten-bewerbung-1030x472.jpg')
 # file.send_keys('1.jpg')
 sleep(3)
 driver.find_element_by_xpath('//*[@id="content"]/div/div/div/section[3]/div/div/div/div/div/div/div/div/div/div/div/button[1]').click()
