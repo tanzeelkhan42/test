365b1adfe9ebfc0ea20d0b4193c84c3f8a7664b3
unknown <f158140@nu.edu.pk>
2020-08-11 16:02:31 +0500

    make it better

--- heroku-test.py
+++ heroku-test.py
@@ -3,6 +3,7 @@ from time import sleep
 from selenium import webdriver
 import os
 
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -16,25 +17,39 @@ chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 # driver.get('https://www.google.com/')
 # print(driver.page_source)
+# driver = webdriver.Chrome(chrome_path, chrome_options=options)
 driver.implicitly_wait(3)
 driver.get("https://jumpstory.com/remove-background/")
-sleep(2)
+# sleep(2)
 # temp = driver.find_elements_by_xpath('//*[@id="page-content"]/div[1]/div/div/div[1]/div/div[1]')
 
-WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="content"]/div/div/div/section[3]/div/div/div/div/div/div/div/div/div/div/div/p[1]/label/input')))
 
-file=driver.find_element_by_xpath('//*[@id="content"]/div/div/div/section[3]/div/div/div/div/div/div/div/div/div/div/div/p[1]/label/input').send_keys(os.getcwd()+'/beautiful-1687981_1920.jpg')
+WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH,
+                                                               '//*[@id="content"]/div/div/div/section[3]/div/div/div/div/div/div/div/div/div/div/div/p[1]/label/input')))
+
+file = driver.find_element_by_xpath(
+    '//*[@id="content"]/div/div/div/section[3]/div/div/div/div/div/div/div/div/div/div/div/p[1]/label/input').send_keys(
+    os.getcwd() + '/beautiful-1274361_1920.jpg')
 # file.send_keys('1.jpg')
 print("file uploaded")
-sleep(20)
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#content > div > div > div > section.elementor-element.elementor-element-aac8557.elementor-section-boxed.elementor-section-height-default.elementor-section-height-default.elementor-section.elementor-top-section > div > div > div > div > div > div > div > div > div > div > div > button.btn.btn-blue'))).click()
+# sleep(10)
+element = driver.find_element_by_class_name("elementor-button-wrapper")
+actions = ActionChains(driver)
+actions.move_to_element(element).perform()
+# sleep(3)
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR,
+                                                            '#content > div > div > div > section.elementor-element.elementor-element-aac8557.elementor-section-boxed.elementor-section-height-default.elementor-section-height-default.elementor-section.elementor-top-section > div > div > div > div > div > div > div > div > div > div > div > button.btn.btn-blue'))).click()
 
 # WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="content"]/div/div/div/section[3]/div/div/div/div/div/div/div/div/div/div/div/button[1]'))).click()
 # WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="content"]/div/div/div/section[3]/div/div/div/div/div/div/div/div/div/div/div/button[1]'))).click()
 print("file bg removing")
 # driver.find_element_by_xpath('//*[@id="content"]/div/div/div/section[3]/div/div/div/div/div/div/div/div/div/div/div/button[1]').click()
 # sleep(10)
-WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, '//*[@id="content"]/div/div/div/section[3]/div/div/div/div/div/div/div/div/div/div/div/a/span'))).click()
+wait = WebDriverWait(driver, 30)
+element = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="content"]/div/div/div/section[3]/div/div/div/div/div/div/div/div/div/div/div/a/span')))
+element.click()
+# WebDriverWait(driver, 20).until(EC.presence_of_element_located(
+#     (By.XPATH, '//*[@id="content"]/div/div/div/section[3]/div/div/div/div/div/div/div/div/div/div/div/a/span'))).click()
 print("file bg downloaded")
 # driver.find_element_by_xpath('//*[@id="content"]/div/div/div/section[3]/div/div/div/div/div/div/div/div/div/div/div/a/span').click()
 # sleep(4)
