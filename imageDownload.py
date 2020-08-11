15e3f85d03e1a9e453427aaad13f59a318793c4e
unknown <f158140@nu.edu.pk>
2020-08-11 14:51:56 +0500

    make it better

--- imageDownload.py
+++ imageDownload.py
@@ -17,8 +17,11 @@ WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@i
 file=driver.find_element_by_xpath('//*[@id="content"]/div/div/div/section[3]/div/div/div/div/div/div/div/div/div/div/div/p[1]/label/input').send_keys(os.getcwd()+'/beautiful-1687981_1920.jpg')
 # file.send_keys('1.jpg')
 print("file uploaded")
-sleep(10)
-WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="content"]/div/div/div/section[3]/div/div/div/div/div/div/div/div/div/div/div/button[1]'))).click()
+sleep(5)
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#content > div > div > div > section.elementor-element.elementor-element-aac8557.elementor-section-boxed.elementor-section-height-default.elementor-section-height-default.elementor-section.elementor-top-section > div > div > div > div > div > div > div > div > div > div > div > button.btn.btn-blue'))).click()
+
+# WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="content"]/div/div/div/section[3]/div/div/div/div/div/div/div/div/div/div/div/button[1]'))).click()
+# WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="content"]/div/div/div/section[3]/div/div/div/div/div/div/div/div/div/div/div/button[1]'))).click()
 print("file bg removing")
 # driver.find_element_by_xpath('//*[@id="content"]/div/div/div/section[3]/div/div/div/div/div/div/div/div/div/div/div/button[1]').click()
 # sleep(10)
