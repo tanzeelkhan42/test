1ad2b5af89b746ee0d4a435162569b285357937c
unknown <f158140@nu.edu.pk>
2020-08-11 14:25:43 +0500

    make it better

--- heroku-test.py
+++ heroku-test.py
@@ -3,6 +3,7 @@ from time import sleep
 from selenium import webdriver
 import os
 
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
