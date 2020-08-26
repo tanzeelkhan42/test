b579e8b6b243988f7c1dd40bc1f58094de6cd73c
unknown <f158140@nu.edu.pk>
2020-08-26 11:15:04 +0500

    make it better

--- app.py
+++ app.py
@@ -151,7 +151,7 @@ def file():
             chrome_options.add_experimental_option("prefs", prefs)
             chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
             chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-
+            chrome_options.binary_location = os.environ.get("CHROMEDRIVER_PATH")
             chrome_options.add_argument("--headless")
             chrome_options.add_argument("--disable-dev-shm-usage")
             chrome_options.add_argument("--no-sandbox")
@@ -206,4 +206,4 @@ def file():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0',port=80)
